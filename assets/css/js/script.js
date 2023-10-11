e251178548a50cb4ae9d7038808d7a53a1b13a8d
benaicha mohammed <benaichamohammed@outlook.fr>
2023-10-11 16:02:13 +0200

    comitto

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -1,1 +1,25 @@
 console.log("scipt chargé")
+
+// creer un element de table//
+// On créé un élément HTML "main"
+let divCalculatrice = document.createElement("main");
+document.body.appendChild("main")
+
+// On lui attribut des caractéristiques (id, class, text, couleur, etc)
+divCalculatrice.setAttribute("id", "calculatrice");
+// divCalculatrice.classList.add("")
+
+// On injecte l'élément en tant qu'enfant de "body"
+document.body.appendChild(divCalculatrice);
+
+// On créé un élément HTML "div"
+let calcEcran = document.createElement("div");
+
+// On lui attribut des caractéristiques (id, class, text, couleur, etc)
+calcEcran.innerText = "Écran de ma calculette";
+
+// On injecte l'élément en tant qu'enfant de "divCalculatrice"
+divCalculatrice.appendChild(calcEcran);
+
+// document.querySelector("#calculatrice").appendChild(calcEcran);
+
